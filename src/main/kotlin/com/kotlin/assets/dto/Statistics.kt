5204eff2b4392c7dbadc423f0c4a5f07b0e837fc
MykhailoYablon <mihayablon@gmail.com>
2026-02-22 12:24:05 +0200

    Added separate statistics.html

---
+++ src/main/kotlin/com/kotlin/assets/dto/Statistics.kt
@@ -0,0 +1,25 @@
+package com.kotlin.assets.dto
+
+import java.math.BigDecimal
+import java.time.Month
+
+data class Statistics(
+    val grandTotal: BigDecimal,
+    val grandUsdTotal: BigDecimal,
+    val byYear: List<YearSummary>
+)
+
+data class MonthSummary(
+    val month: Month,
+    val total: BigDecimal,
+    val usdTotal: BigDecimal,
+    val count: Int
+)
+
+data class YearSummary(
+    val year: Int,
+    val total: BigDecimal,
+    val usdTotal: BigDecimal,
+    val count: Int,
+    val byMonth: List<MonthSummary>
+)
